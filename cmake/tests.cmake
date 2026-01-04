c67a2bc678bc475a651a024dfe9db2e062054c8e
Bahman Movaqar <bahman@bahmanm.com>
2026-01-04 12:37:24 -0800

    [#27] Enable linter in the pipeline (#31)

---
+++ cmake/tests.cmake
@@ -0,0 +1,40 @@
+enable_testing()
+
+### gtest
+add_subdirectory(gtest)
+
+add_custom_target(scid_test
+        COMMAND "${CMAKE_CTEST_COMMAND}" --output-on-failure -R "^gtest$"
+        DEPENDS scid_test_bin
+        WORKING_DIRECTORY "${CMAKE_BINARY_DIR}"
+        COMMENT "Building and running GoogleTest"
+)
+
+add_test(
+        NAME scid_test
+        COMMAND $<TARGET_FILE:scid_test_bin>
+)
+
+### tcl_test
+set(TCL_TEST_RUNNER "${CMAKE_SOURCE_DIR}/tcl/tests/run_all_tests.tcl")
+
+add_custom_target(tcl_test
+        COMMAND
+        "${CMAKE_COMMAND}" -E chdir "${CMAKE_SOURCE_DIR}"
+        "${TCL_TCLSH}" "${TCL_TEST_RUNNER}"
+        COMMENT "Running Tcl tests"
+)
+
+add_test(
+        NAME tcl_test
+        COMMAND
+        "${CMAKE_COMMAND}" -E chdir "${CMAKE_SOURCE_DIR}"
+        "${TCL_TCLSH}" "${TCL_TEST_RUNNER}"
+)
+
+get_filename_component(_tcl_bin_dir "${TCL_TCLSH}" DIRECTORY)
+get_filename_component(_tcl_lib_dir "${TCL_LIBRARY}" DIRECTORY)
+set_tests_properties(tcl_test
+        PROPERTIES
+        ENVIRONMENT "PATH=${_tcl_bin_dir}:$ENV{PATH};LD_LIBRARY_PATH=${_tcl_lib_dir}:$ENV{LD_LIBRARY_PATH}"
+)
--- gtest/test_game.cpp
+++ gtest/test_game.cpp
@@ -249,7 +249,7 @@ TEST(Test_Game, encodeFEN) {
 		game.DecodeMovesOnly(bbuf);
 		game.MoveToStart();
 		char str[1024];
-		game.currentPos()->PrintFEN(str);
+		game.currentPos()->PrintFEN(str, sizeof(str));
 		EXPECT_STREQ(kiwipete, str);
 	}
 }
@@ -314,11 +314,11 @@ TEST(Test_Game, illegalPGN_Castling) {
 	EXPECT_FALSE(pgnLog.log.empty());
 	char fen[256];
 	game.MoveToEnd();
-	game.currentPos()->PrintFEN(fen);
+	game.currentPos()->PrintFEN(fen, sizeof(fen));
 	EXPECT_STREQ(
 	    fen, "rnbq1rk1/ppppbppp/5n2/4p3/4P3/5N2/PPPPBPPP/RNBQ1RK1 w - - 6 5");
 	game.MoveBackup();
-	game.currentPos()->PrintFEN(fen);
+	game.currentPos()->PrintFEN(fen, sizeof(fen));
 	EXPECT_STREQ(
 	    fen, "rnbqk2r/ppppbppp/5n2/4p3/4P3/5N2/PPPPBPPP/RNBQ1RK1 b kq - 5 4");
 }
@@ -331,7 +331,7 @@ TEST(Test_Game, illegalPGN_KingCapture) {
 	EXPECT_FALSE(pgnLog.log.empty());
 	char fen[256];
 	game.MoveToEnd();
-	game.currentPos()->PrintFEN(fen);
+	game.currentPos()->PrintFEN(fen, sizeof(fen));
 	EXPECT_STREQ(
 	    fen, "rnbqk1nr/pppp1ppp/4p3/8/1b1PP3/8/PPP2PPP/RNBQKBNR w KQkq - 1 3");
 }
