cf38239424f9ecad26110c84e337d2e4e2466918
Bahman Movaqar <Bahman@BahmanM.com>
2026-01-04 10:46:40 -0800

    Configure ctest instead of relying on shell

--- cmake/tests.cmake
+++ cmake/tests.cmake
@@ -31,3 +31,10 @@ add_test(
         "${CMAKE_COMMAND}" -E chdir "${CMAKE_SOURCE_DIR}"
         "${TCL_TCLSH}" "${TCL_TEST_RUNNER}"
 )
+
+get_filename_component(_tcl_bin_dir "${TCL_TCLSH}" DIRECTORY)
+get_filename_component(_tcl_lib_dir "${TCL_LIBRARY}" DIRECTORY)
+set_tests_properties(tcl_test
+        PROPERTIES
+        ENVIRONMENT "PATH=${_tcl_bin_dir}:$ENV{PATH};LD_LIBRARY_PATH=${_tcl_lib_dir}:$ENV{LD_LIBRARY_PATH}"
+)
