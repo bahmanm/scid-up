18688d6486690abeaf80a8640c7bfe8361e9ef4e
Bahman Movaqar <Bahman@BahmanM.com>
2026-01-04 14:36:07 -0800

    Rename `scid_test` to `cpp_test`

--- cmake/tests.cmake
+++ cmake/tests.cmake
@@ -3,41 +3,33 @@ if(NOT BUILD_TESTING)
   return()
 endif()
 
-### gtest
+### cpp_test
 add_subdirectory(gtest)
 
-add_custom_target(scid_test
-        COMMAND "${CMAKE_CTEST_COMMAND}" --output-on-failure -R "^scid_test$"
-        DEPENDS scid_test_bin
-        WORKING_DIRECTORY "${CMAKE_BINARY_DIR}"
-        COMMENT "Building and running GoogleTest"
-)
-
 add_test(
-        NAME scid_test
-        COMMAND $<TARGET_FILE:scid_test_bin>
+  NAME cpp_test
+  COMMAND $<TARGET_FILE:scid_test_bin>
+)
+set_tests_properties(
+  cpp_test
+  PROPERTIES
+  LABELS "cpp;gtest"
 )
 
 ### tcl_test
 set(TCL_TEST_RUNNER "${CMAKE_SOURCE_DIR}/tcl/tests/run_all_tests.tcl")
 
-add_custom_target(tcl_test
-        COMMAND
-        "${CMAKE_COMMAND}" -E chdir "${CMAKE_SOURCE_DIR}"
-        "${TCL_TCLSH}" "${TCL_TEST_RUNNER}"
-        COMMENT "Running Tcl tests"
-)
-
 add_test(
-        NAME tcl_test
-        COMMAND
-        "${CMAKE_COMMAND}" -E chdir "${CMAKE_SOURCE_DIR}"
-        "${TCL_TCLSH}" "${TCL_TEST_RUNNER}"
+  NAME tcl_test
+  COMMAND
+  "${CMAKE_COMMAND}" -E chdir "${CMAKE_SOURCE_DIR}"
+  "${TCL_TCLSH}" "${TCL_TEST_RUNNER}"
 )
 
 get_filename_component(_tcl_bin_dir "${TCL_TCLSH}" DIRECTORY)
 get_filename_component(_tcl_lib_dir "${TCL_LIBRARY}" DIRECTORY)
 set_tests_properties(tcl_test
-        PROPERTIES
-        ENVIRONMENT "PATH=${_tcl_bin_dir}:$ENV{PATH};LD_LIBRARY_PATH=${_tcl_lib_dir}:$ENV{LD_LIBRARY_PATH}"
+  PROPERTIES
+  ENVIRONMENT "PATH=${_tcl_bin_dir}:$ENV{PATH};LD_LIBRARY_PATH=${_tcl_lib_dir}:$ENV{LD_LIBRARY_PATH}"
+  LABELS "tcl"
 )
