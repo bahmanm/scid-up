a160838262d890944442d1f1827241806b76152a
Bahman Movaqar <Bahman@BahmanM.com>
2026-01-04 16:37:23 -0800

    Extract main binary to a dedicated file

---
+++ cmake/bins/main.cmake
@@ -0,0 +1,43 @@
+# Main executable (ScidUp).
+
+file(GLOB SCIDUP_MAIN_SOURCES
+  "${CMAKE_SOURCE_DIR}/src/*.h"
+  "${CMAKE_SOURCE_DIR}/src/*.cpp"
+)
+
+if(MSVC)
+  add_executable(
+    scidup_main
+    WIN32
+    ${SCIDUP_MAIN_SOURCES}
+    "${CMAKE_SOURCE_DIR}/resources/win/scid.rc"
+    "${CMAKE_SOURCE_DIR}/resources/win/scid.manifest"
+  )
+  target_link_options(scidup_main PRIVATE /ENTRY:mainCRTStartup)
+else()
+  add_executable(scidup_main ${SCIDUP_MAIN_SOURCES})
+endif()
+
+add_executable(ScidUp::Bins::Main ALIAS scidup_main)
+set_target_properties(scidup_main PROPERTIES OUTPUT_NAME "scid-up")
+set_property(TARGET scidup_main PROPERTY INTERPROCEDURAL_OPTIMIZATION_RELEASE True)
+target_link_libraries(scidup_main PRIVATE ScidUp::Libs::Polyglot Threads::Threads ScidUp::Libs::Tcl)
+
+option(SPELLCHKVALIDATE "Verify the integrity of spelling files" OFF)
+if(SPELLCHKVALIDATE)
+  target_compile_definitions(scidup_main PRIVATE SPELLCHKVALIDATE)
+endif()
+
+install(TARGETS scidup_main DESTINATION scid-up)
+
+file(GLOB ECO_FILES "${CMAKE_SOURCE_DIR}/*.eco")
+install(FILES ${ECO_FILES} DESTINATION scid-up)
+install(DIRECTORY "${CMAKE_SOURCE_DIR}/bitmaps" DESTINATION scid-up)
+install(DIRECTORY "${CMAKE_SOURCE_DIR}/bitmaps2" DESTINATION scid-up)
+install(DIRECTORY "${CMAKE_SOURCE_DIR}/books" DESTINATION scid-up)
+install(DIRECTORY "${CMAKE_SOURCE_DIR}/html" DESTINATION scid-up)
+install(DIRECTORY "${CMAKE_SOURCE_DIR}/img" DESTINATION scid-up)
+install(DIRECTORY "${CMAKE_SOURCE_DIR}/scripts" DESTINATION scid-up)
+install(DIRECTORY "${CMAKE_SOURCE_DIR}/sounds" DESTINATION scid-up)
+install(DIRECTORY "${CMAKE_SOURCE_DIR}/tcl" DESTINATION scid-up)
+
