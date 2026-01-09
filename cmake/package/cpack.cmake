4b255e7113a25ba749405b03c47cb9ce4e54c3cd
Bahman Movaqar <Bahman@BahmanM.com>
2026-01-09 07:47:35 -0800

    Define the layout and include the artefacts in the archive

---
+++ cmake/package/cpack.cmake
@@ -0,0 +1,6 @@
+set( CPACK_PACKAGE_NAME "scid-up" )
+set( CPACK_PACKAGE_VERSION "${PROJECT_VERSION}" )
+set( CPACK_PACKAGE_DESCRIPTION_SUMMARY "Chess database application with play and training functionality" )
+
+include( CPack )
+
---
+++ cmake/package/portable/layout.cmake
@@ -0,0 +1,37 @@
+option(
+    SCIDUP_PORTABLE_ARCHIVE
+    "Configure installation directories for a portable archive (./bin, ./lib, ./share)."
+    OFF )
+
+if( NOT SCIDUP_PORTABLE_ARCHIVE )
+    return()
+endif()
+
+set(
+    CMAKE_INSTALL_PREFIX
+    "/"
+    CACHE PATH
+    "Install prefix for portable archives."
+    FORCE )
+
+set(
+    CMAKE_INSTALL_BINDIR
+    "bin"
+    CACHE PATH
+    "Executable directory for portable archives."
+    FORCE )
+
+set(
+    CMAKE_INSTALL_LIBDIR
+    "lib"
+    CACHE PATH
+    "Library directory for portable archives."
+    FORCE )
+
+set(
+    CMAKE_INSTALL_DATADIR
+    "share"
+    CACHE PATH
+    "Data directory for portable archives."
+    FORCE )
+
