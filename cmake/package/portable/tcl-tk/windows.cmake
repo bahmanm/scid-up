e68c3f69872740f8ef06c23cefef46c0a3e3095b
Bahman Movaqar <Bahman@BahmanM.com>
2026-01-17 10:09:05 -0800

    Use the library names instead of globbing

--- cmake/package/portable/tcl-tk/windows.cmake
+++ cmake/package/portable/tcl-tk/windows.cmake
@@ -1,28 +1,28 @@
 # Bundle Tcl/Tk DLLs next to the executable for portable archives.
-file(
-    GLOB
-    _scidup_tcl_dlls
-    CONFIGURE_DEPENDS
-    "${SCIDUP_TCL_TK_PREFIX}/bin/tcl*.dll" )
-file(
-    GLOB
-    _scidup_tk_dlls
-    CONFIGURE_DEPENDS
-    "${SCIDUP_TCL_TK_PREFIX}/bin/tk*.dll" )
+#
+# The DLL names are derived from the import libraries as reported by find_package(TCL).
+# For instance, based on ".../lib/tcl9tk90.lib", the ".../bin/tcl9tk90.dll" name is assumed.
 
-if( NOT _scidup_tcl_dlls )
+get_filename_component( _scidup_tcl_import_library_name "${TCL_LIBRARY}" NAME_WE )
+set( _scidup_tcl_dll "${SCIDUP_TCL_TK_PREFIX}/bin/${_scidup_tcl_import_library_name}.dll" )
+if( NOT EXISTS "${_scidup_tcl_dll}" )
     message(
         FATAL_ERROR
-        "Failed to locate Tcl DLLs under ${SCIDUP_TCL_TK_PREFIX}/bin.\n"
-        "Expected a match for: ${SCIDUP_TCL_TK_PREFIX}/bin/tcl*.dll\n" )
+        "Failed to locate Tcl DLL: ${_scidup_tcl_dll}\n"
+        "TCL_LIBRARY=${TCL_LIBRARY}\n"
+        "SCIDUP_TCL_TK_PREFIX=${SCIDUP_TCL_TK_PREFIX}\n" )
 endif()
-if( NOT _scidup_tk_dlls )
+
+get_filename_component( _scidup_tk_import_library_name "${TK_LIBRARY}" NAME_WE )
+set( _scidup_tk_dll "${SCIDUP_TCL_TK_PREFIX}/bin/${_scidup_tk_import_library_name}.dll" )
+if( NOT EXISTS "${_scidup_tk_dll}" )
     message(
         FATAL_ERROR
-        "Failed to locate Tk DLLs under ${SCIDUP_TCL_TK_PREFIX}/bin.\n"
-        "Expected a match for: ${SCIDUP_TCL_TK_PREFIX}/bin/tk*.dll\n" )
+        "Failed to locate Tk DLL: ${_scidup_tk_dll}\n"
+        "TK_LIBRARY=${TK_LIBRARY}\n"
+        "SCIDUP_TCL_TK_PREFIX=${SCIDUP_TCL_TK_PREFIX}\n" )
 endif()
 
 install(
-    FILES ${_scidup_tcl_dlls} ${_scidup_tk_dlls}
+    FILES "${_scidup_tcl_dll}" "${_scidup_tk_dll}"
     DESTINATION "${CMAKE_INSTALL_BINDIR}" )
