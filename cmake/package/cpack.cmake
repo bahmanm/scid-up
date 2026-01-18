4b260bc93f3f65a5ae31361611bc78d272538452
Bahman Movaqar <bahman@bahmanm.com>
2026-01-18 11:15:13 -0800

    [#80] Executable name and icon (#81)

--- cmake/package/cpack.cmake
+++ cmake/package/cpack.cmake
@@ -1,6 +1,6 @@
 set( CPACK_PACKAGE_NAME "scid-up" )
 set( CPACK_PACKAGE_VERSION "${PROJECT_VERSION}" )
-set( CPACK_PACKAGE_DESCRIPTION_SUMMARY "Chess database application with play and training functionality" )
+set( CPACK_PACKAGE_DESCRIPTION_SUMMARY "Cross-Platform Chess Database and Analysis GUI" )
 
 include( CPack )
 
